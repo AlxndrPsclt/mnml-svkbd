43417965f146a6d6ac22d2f3bb90a409032bc1fb
alxndrpsclt <alxndr.psclt@gmail.com>
2021-01-11 00:01:50 +0100

    Changes font to beloved Iosevka

--- config.def.h
+++ config.def.h
@@ -1,9 +1,9 @@
 static const Bool wmborder = True;
 static int fontsize = 20;
-static double overlay_delay = 1.0;
+static double overlay_delay = 0.5;
 static int heightfactor = 16; //one row of keys takes up 1/x of the screen height
 static const char *fonts[] = {
-	"DejaVu Sans:bold:size=20"
+	"Iosevka:bold:size=20"
 };
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
